c7b164028f6d7b82c76f589d08ef4002c11df796
ac0x <acom20021@gmail.com>
2023-12-17 23:01:47 +0100

    dodat ukupan broj skeniranih studenata sa opcijom reset

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, url_for
+from flask_socketio import SocketIO, emit
 import cv2
 import face_recognition
 import numpy as np
@@ -6,9 +7,11 @@ import os
 import pickle
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(1)
+socketio = SocketIO(app)
+camera = cv2.VideoCapture(0)
 
-
+recognized_faces = set()
+present_students_count = 0
 
 print("Loading encoded file")
 file = open("EncodeFile.p", 'rb')
@@ -30,14 +33,19 @@ def recognize_faces(frame):
 
         matchIndex = np.argmin(faceDistance)
 
-        if matches[matchIndex]:
+        if matches[matchIndex] and studentIds[matchIndex] not in recognized_faces:
             print("Known face detected")
             print(f"Student ID: {studentIds[matchIndex]}")
+
+            recognized_faces.add(studentIds[matchIndex])
+
+            global present_students_count
+            present_students_count += 1
 
             print(f"FaceLocation: {faceLoc}")
 
             top, right, bottom, left = faceLoc
-            bbox = (left * 4 + 55, top * 4 + 162, (right - left) * 4, (bottom - top) * 4)
+            bbox = (left * 4, top * 4, (right - left) * 4, (bottom - top) * 4)
 
             cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), (0, 255, 0), 2)
 
@@ -52,8 +60,18 @@ def generate_frames():
             img = recognize_faces(img)
             _, jpeg = cv2.imencode('.jpg', img)
             frame = jpeg.tobytes()
+
+            socketio.emit('update_present_students_count', present_students_count, namespace='/')
+
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@socketio.on('reset_present_students_count', namespace='/')
+def handle_reset_present_students_count():
+    global present_students_count
+    present_students_count = 0
+    recognized_faces.clear()
+    #print("Reseting")
 
 @app.route('/')
 def index():
@@ -64,4 +82,4 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
